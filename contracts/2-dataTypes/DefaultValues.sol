eba298fd3fb1c50c6ee5ea006e5df8f12a041415
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-07-30 00:11:48 +0400

    🚧 default values

---
+++ contracts/2-dataTypes/DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DefaultValues {
+    uint public u; // 0
+    address public a; // 0x0000000000000000000000000000000000000000
+    bool public b; // false
+    int public i; // 0
+    bytes32 public b32; // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
