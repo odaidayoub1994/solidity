e6b35fc7fa576c67d3aeccaf467a7b79fb910730
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-07-30 13:16:34 +0400

    🚧 define constant values

---
+++ contracts/2-dataTypes/constants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract ValueTypes {
+    address public constant MY_ADDRESS = 0x03a02F3De9bbf0C29cb9C318b132aE5614390807;
+    uint public constant MY_AGE = 29;
+}
